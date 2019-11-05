331a9bf3ccc3d3c4434f8e7b342c2b926958d33e
BrunoE-HG <45363021+BrunoE-HG@users.noreply.github.com>
2019-11-05 14:17:23 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from glob import glob
 from subprocess import call
+from os import remove
 
 bad_file_extensions=(".avi", ".mkv")
 
-folder_path=r"C:\Users\bruno\Desktop\anomalous\shared 2"
+#folder_path=r"C:\Users\bruno\Desktop\anomalous\shared 2"
+folder_path=r"C:\Users\bruno\Desktop\resilio shared"
 
 folder_path+=r"\**\*"
 
@@ -13,3 +15,4 @@ for extension in bad_file_extensions:files.extend(glob(folder_path+extension, re
 for source in files:
     for ext in bad_file_extensions:dest=source.replace(ext,".mp4")
     call(("HandBrakeCLI", "-i", source, "-o", dest))
+    remove(source)
