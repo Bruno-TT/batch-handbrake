17bff39daf07327a18bc9f3678ae5acda0159597
Bruno-TT <33663538+Bruno-TT@users.noreply.github.com>
2019-11-05 14:14:41 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from glob import glob
+from subprocess import call
+
+bad_file_extensions=(".avi", ".mkv")
+
+folder_path=r"C:\Users\bruno\Desktop\anomalous\shared 2"
+
+folder_path+=r"\**\*"
+
+files=[]
+for extension in bad_file_extensions:files.extend(glob(folder_path+extension, recursive=True))
+
+for source in files:
+    for ext in bad_file_extensions:dest=source.replace(ext,".mp4")
+    call(("HandBrakeCLI", "-i", source, "-o", dest))
